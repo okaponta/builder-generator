744fa48f48c68a813366c939b08f43b9aef43027
Kohei Okamoto <okaponta.19910919@gmail.com>
2023-06-12 14:13:58 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # builder-generator
+
+Builder Generator for Kotlin data class.
+
+# Install
+
+- git clone https://github.com/okaponta/builder-generator.git
+- brew install kotlin
+
+# Usage
+
+- kotlin generator.kts -- -arg "path to your kotlin data file"
+
+# Recommended Usage
+
+- alias ktgenerate='(){pushd /path/to/repo;kotlin generator.kts -- -arg $1 | pbcopy;popd}'
+- press Command+Shift+C to get your path to file
+- enter `ktgenerate (paste path)`
+- generated builder is now on your clipboard!!
---
+++ generator.kts
@@ -0,0 +1,59 @@
+import java.io.File
+import java.util.Scanner
+
+data class Field(
+    val name: String,
+    val type: String,
+    val nullable: Boolean,
+) {
+    fun default(): String {
+        return when {
+            nullable -> "null"
+            type.contains("String") -> """"""""
+            else -> "0"
+        }
+    }
+}
+
+val fileName = args[1]
+
+var className = ""
+val classNameRegex = "data class (.*)\\(".toRegex()
+
+val fields = mutableListOf<Field>()
+val fieldRegex = ".*val (.*): (Byte|Byte\\?|Int|Int\\?|String|String\\?).*".toRegex()
+
+val sc = Scanner(File(fileName))
+while (sc.hasNextLine()) {
+    val line = sc.nextLine()
+    if (classNameRegex.matches(line)) {
+        className = line.replace(classNameRegex, "$1")
+    }
+    if (fieldRegex.matches(line)) {
+        val fieldName = line.replace(fieldRegex, "$1")
+        val fieldType = line.replace(fieldRegex, "$2")
+        val isNullable = fieldType.contains('?')
+        fields.add(Field(fieldName, fieldType, isNullable))
+    }
+}
+
+println("class " + className + "Builder {")
+for (field in fields) {
+    println("    private var " + field.name + ": " + field.type + " = " + field.default())
+}
+println()
+for (field in fields) {
+    println("    fun ${field.name}(${field.name}: ${field.type}): $className" + "Builder {")
+    println("        this.${field.name} = ${field.name}")
+    println("        return this")
+    println("    }")
+    println()
+}
+println("    fun build(): $className {")
+println("        return $className (")
+for (field in fields) {
+    println("            ${field.name}, ")
+}
+println("        )")
+println("    }")
+println("}")
